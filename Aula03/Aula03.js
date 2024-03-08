7e418dbf271f069960c3c220e184fcdc35f881ac
maikelspd <162368668+maikelspd@users.noreply.github.com>
2024-03-08 17:11:51 -0300

    Add files via upload

---
+++ Aula03/Aula03.js
@@ -0,0 +1,7 @@
+//Este código imprime uma mensagem importante
+const meuNome='Maikel Sander padilha dos Santos'
+const minhaIdade=15
+
+console.log("Meu nome é ",meuNome,"e tenho ",minhaIdade,"anos")
+
+
---
+++ Aula03/conversao.js
@@ -0,0 +1,4 @@
+const numero1 = '5'
+const numero2 = '2'
+
+console.log(Number(numero1))
