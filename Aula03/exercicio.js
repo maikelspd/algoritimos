047ced6dbcd2461c121e53640288c58765622a8a
maikelspd <162368668+maikelspd@users.noreply.github.com>
2024-03-08 17:26:16 -0300

    Add files via upload

---
+++ Aula03/exercicio.js
@@ -0,0 +1,7 @@
+//Este código imprime uma mensagem importante
+const meuNome="Maikel"
+const sobrenome="padilha"
+const minhaIdade=15
+const souEstudante=true
+
+console.log("Meu nome é",meuNome,sobrenome,"eu tenho",minhaIdade,"anos,sou estudante?",souEstudante)
